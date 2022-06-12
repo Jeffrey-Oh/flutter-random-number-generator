759caf5ece00171e1241569b2331152c0f5a2747
오태현 <jeffrey@kai-i.com>
2022-06-13 02:28:12 +0900

    Slider 위젯 사용하기

--- lib/screen/settings/SettingsScreen.dart
+++ lib/screen/settings/SettingsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color/Color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,12 +9,50 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Settings Screen'),
-      ),
-    );
+        backgroundColor: PRIMARY_COLOR,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: maxNumber
+                        .toInt()
+                        .toString()
+                        .split('')
+                        .map((e) => Image.asset(
+                              'assets/img/$e.png',
+                              height: 70,
+                              width: 50,
+                            ))
+                        .toList(),
+                  ),
+                ),
+                Slider(
+                  value: maxNumber,
+                  min: 10000,
+                  max: 1000000,
+                  onChanged: (double val) {
+                    setState(() {
+                      maxNumber = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: RED_COLOR),
+                        onPressed: () {},
+                        child: Text('저장!')))
+              ],
+            ),
+          ),
+        ));
   }
 }
