054f906a597561188c075e252e5ccd7ef8e500f4
오태현 <jeffrey@kai-i.com>
2022-06-12 23:10:08 +0900

    디자인 적용하기

--- lib/screen/home/HomeScreen.dart
+++ lib/screen/home/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color/Color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,35 +12,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤숫자 생성기'),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text('123'), Text('456'), Text('789')],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '랜덤숫자 생성기',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.settings,
+                      color: RED_COLOR,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            // 버튼의 사이즈를 양 끝으로 늘리는 방법
-            // 1. SizedBox 로 감싼 후 width 를 double.infinity 속성을 부여한다
-            // 2. 전체를 Row로 감싼 후 안의 내용을 Expanded로 감싼다
-            // 3. Container로 감싼 후 width 를 double.infinity 속성을 부여한다
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('생성하기'),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text('123'), Text('456'), Text('789')],
+                ),
               ),
-            ),
-          ],
+              // 버튼의 사이즈를 양 끝으로 늘리는 방법
+              // 1. SizedBox 로 감싼 후 width 를 double.infinity 속성을 부여한다
+              // 2. 전체를 Row로 감싼 후 안의 내용을 Expanded로 감싼다
+              // 3. Container로 감싼 후 width 를 double.infinity 속성을 부여한다
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: RED_COLOR),
+                  onPressed: () {},
+                  child: Text('생성하기'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
